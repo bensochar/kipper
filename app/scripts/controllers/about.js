6b20b6ef29fdb408562ccaa374159d557f5d968b
ben sochar <bsochar@gmail.com>
2016-08-30 23:53:20 -0400

    yeoman angular generator, updated gitignore

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name klipperApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the klipperApp
+ */
+angular.module('klipperApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name klipperApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the klipperApp
+ */
+angular.module('klipperApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
